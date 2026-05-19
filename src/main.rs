ddace1eca4751dc7d243bb55844d5204e021888d
Perry Clayton <perryclayton951@hotmail.com>
2026-05-19 01:29:39 +0000

    Crates added. Make a request.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let url = "http://localhost:8080";
+    let body = reqwest::get(url).await.unwrap().text().await.unwrap();
+    println!("{}", body);
 }
+
